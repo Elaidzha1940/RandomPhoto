67cac2f54be0b320815250e66914f19457cc46dd
Elaidzha1940 <eli080293@gmail.com>
2023-05-26 18:22:58 +0700

    RandomPhoto

---
+++ RandomPhoto/ViewController.swift
@@ -0,0 +1,21 @@
+//  /*
+//
+//  Project: RandomPhoto
+//  File: ViewController.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 26.05.2023
+//
+//  Status
+//
+//  */
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
